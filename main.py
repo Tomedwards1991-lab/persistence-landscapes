98c05075d53b8c263571447ee935aa470129841d
Thomas J Edwards <edwardstj1@cardiff.ac.uk>
2017-12-29 19:06:17 +0000

    main has been updated to be used with external data

--- main.py
+++ main.py
@@ -1,54 +1,111 @@
+import math
 import numpy
 import grid_simplex
 import zig_zag_homology
+import csv
+import subprocess
+from scipy import stats
+import pyproj
+import matplotlib.pyplot as plt
+
+# Read in the csv file and store the coordiantes in a list
+l1 = []
+l2 = []
+yearL = []
+new_list1 = []
+new_list2 = []
+new_yearL = []
+'''
+with open('./samplep.csv', 'rU') as csvfile:
+    mycsv = csv.reader(csvfile)
+    for row in mycsv:
+        coord1 = row[22]
+        coord2 = row[23]
+        year = row[34]
+        month = row[35]
+        if(coord1 != "" and coord2 != "" and year != ""):
+            l1.append(coord1)
+            l2.append(coord2)
+            yearL.append(year)
+
+for item in l1:
+	new_list1.append(float(item))
+
+for item in l2:
+    new_list2.append(float(item))
+
+for item in yearL:
+    new_yearL.append(int(item))
+'''
 
 # Initialize grid of points
-grid_simplex_object = grid_simplex.grid_simplex(5, 10) # a gird of size 5 by 10
+grid_simplex_object = grid_simplex.grid_simplex(100, 100)
 zig_zag_homology_object = zig_zag_homology.zig_zag_homology(grid_simplex_object.simplices)
 
+directoryPath = '/Users/thomasedwards/Desktop/WorkPhDAT/spatial_temporal_topology_library/csvs'
+file_list = subprocess.check_output(['find', directoryPath, '-name', '*.csv']).split('\n')[:-1]
 
-# For times 1, 2 and 3 turn on and off different points in the grid. Plot grid at each time step.
-a = numpy.zeros((5,10), dtype=int)
-a[2,5] = 1
-a[3,5] = 1
-a[2,4] = 1
-a[3,4] = 1
-grid_simplex_object.update_active_simplex(a)
-time = 1
-zig_zag_homology_object.update_simplex_active(grid_simplex_object.simplex_active, time)
-grid_simplex_object.plot_active_simplex()
+for fileCSV in file_list:
+    csvfile = open(fileCSV, 'rU')
+    mycsv = csv.reader(csvfile)
+    for row in mycsv:
+        coord1 = row[22]
+        coord2 = row[23]
+        if (coord1 != "" and coord2 != ""):
+            l1.append(coord1)
+            l2.append(coord2)
 
+    for item in l1:
+        new_list1.append(float(item))
 
-a = numpy.zeros((5,10), dtype=int)
-a[1,9] = 1
-a[2,5] = 1
-a[3,5] = 1
-a[2,4] = 1
-a[2,6] = 1
-grid_simplex_object.update_active_simplex(a)
-time = 2
-zig_zag_homology_object.update_simplex_active(grid_simplex_object.simplex_active, time)
-grid_simplex_object.plot_active_simplex()
+    for item in l2:
+        new_list2.append(float(item))
 
-a = numpy.zeros((5,10), dtype=int)
-a[2,5] = 1
-a[3,5] = 1
-a[2,4] = 1
-a[2,6] = 1
-a[2,7] = 1
-a[3,7] = 1
-a[4,7] = 1
-a[4,6] = 1
-grid_simplex_object.update_active_simplex(a)
-time = 3
-zig_zag_homology_object.update_simplex_active(grid_simplex_object.simplex_active, time)
-grid_simplex_object.plot_active_simplex()
+    # Define projection for the British National Grid
+    bng = pyproj.Proj(init='epsg:27700')
 
+    # do british projection
+    bx, by = bng(new_list1, new_list2)
+
+    m1, m2 = bx, by
+
+    xmin = min(m1)
+    xmax = max(m1)
+    ymin = min(m2)
+    ymax = max(m2)
+
+    # Apply KDE to the coordiantes and prinyt the results on a grid
+    X, Y = numpy.mgrid[xmin:xmax:100j, ymin:ymax:100j]
+    positions = numpy.vstack([X.ravel(), Y.ravel()])
+    values = numpy.vstack([m1, m2])
+    kernel = stats.gaussian_kde(values, bw_method='silverman')
+    kdeOutputArray = numpy.reshape(kernel(positions).T, X.shape)
+
+    # Calculate the mean
+    summ = 0
+    count = 0
+    for x in range(0, len(kdeOutputArray)):
+        for y in range(0, len(kdeOutputArray[0])):
+            summ = summ + kdeOutputArray[x][y]
+            count = count + 1
+
+    mean = summ / count
+
+    # Turn the points on in the grid
+    a = numpy.zeros((len(kdeOutputArray), len(kdeOutputArray[0])), dtype=int)
+    for x in range(0, len(kdeOutputArray)):
+        for y in range(0, len(kdeOutputArray[0])):
+            if kdeOutputArray[x][y] > mean:
+                a[x][y] = 1
+
+    grid_simplex_object.update_active_simplex(a)
+
+    zig_zag_homology_object.update_simplex_active(grid_simplex_object.simplex_active, 3)
+    grid_simplex_object.plot_active_simplex()
 
 # When fihished turn off all points in the grid.
 zig_zag_homology_object.remove_all_simplex()
 zig_zag_homology_object.sort_persistence()
-
 
 # Print results
 print "zero_dim_persistence: ", zig_zag_homology_object.zero_dim_persistence
